9625b02886070dfe8ddbb370ce66327f986d6200
Ginto11 <salinitosnelson@gmail.com>
2025-04-19 00:21:29 -0500

    Ciertos cambios

---
+++ MIAPI/Authentication/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using MIAPI.Data;
+using MIAPI.Models;
+using MIAPI.Services;
+
+namespace MIAPI.Authentication
+{
+    public class AuthService
+    {
+        private readonly UsuarioService service;
+        public AuthService(UsuarioService service)
+        {
+            this.service = service;
+        }
+
+
+        public Task Registrar(Usuario usuario)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task Login(Usuario usuario)
+        {
+            return Task.FromResult(0);
+        }
+
+    }
+}
---
+++ MIAPI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using MIAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MIAPI.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public Task<Usuario> Get()
+        {
+            var usuario = new Usuario { Nombre = "Camilo", Clave ="123", Correo ="x@gmail.com" };
+            return usuario;
+        }
+    }
+}
